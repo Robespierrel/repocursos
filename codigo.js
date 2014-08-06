1ad497d2c30f1068b1cd3317c98e04a937dcb64a
Robespierrel <robespierre25@hotmail.com>
2014-08-06 16:59:18 -0500

    ultima actualizacion
    sdsdsd

---
+++ codigo.js
@@ -0,0 +1,29 @@
+
+
+var peso;
+var pesoEnMarte;// camelcase
+alert ( " quieres saber tu peso en marte);")
+peso = prompt(" cual es tu peso" , "70");
+peso = Number(peso);
+
+pesoEnMarte = ( peso / 9.81) * 3.71
+
+alert( pesoEnMarte );
+
+
+
+
+
+// alert: es una funcion 
+// () : parametros de la funcion
+// "" : cadenas de textos 
+//alert( 1 + "10" );
+
+
+//var nombre = "robespierre";
+//var apellido = "Lugo";
+//var edad = 28;
+
+
+//alert(nombre + " "  + apellido + " \n" + edad + " "  + "años" );
+//alert( "2"  + 5 * 8); 
---
+++ piedra.js
@@ -0,0 +1,10 @@
+var piedra = 0;
+var papel = 1;
+var tijera = 2;
+
+var opcionUsuario;
+var opcionMaquina;
+
+opcionUsuario = prompt("que eliges\npiedra: 0\n papel: 1 \n tijera: 2",);
+
+alert("elegiste: " + opcionUsuario);
